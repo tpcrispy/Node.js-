fc364e2ea2998ac4b88929482080be672e080270
Thomas Smith <thomas.1993.smith@gmail.com>
2016-04-12 16:37:35 +1000

    refactor middleware

--- server.js
+++ server.js
@@ -2,28 +2,19 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next){
-		console.log('private rout hit!');
-		next();
+var middleware = require('./middleware.js');
 
-	},
-	logger: function(req, res, next){
-		console.log('Request: ' + new Date().toString() + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
 
 app.use(middleware.logger);
 
 
-app.get('/about', middleware.requireAuthentication, function(req, res){
+app.get('/about', middleware.requireAuthentication, function(req, res) {
 	res.send("About us");
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, function (){
+app.listen(PORT, function () {
 	console.log('express server started!');
 	console.log('Server running on ' + PORT);
 	console.log('Server running on ' + PORT);
